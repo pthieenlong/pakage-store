a2b44ecf51cabfe64eed5ae7c5a759662d2f2376
pthieenlong <p.thieenlong.304@gmail.com>
2022-02-20 14:13:18 +0700

    responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,101 +1,101 @@
-const modal = document.querySelector('.modal');
-// const userMethods = document.querySelector('.user-methods');
-const loginBtn = document.querySelector('#login-btn');
-const regBtn = document.querySelector('#reg-btn');
-const modalClose = document.querySelector('.modal-close');
-const userMethods = document.querySelector('.user-methods');
-const loginTab = document.querySelector('#login-tab');
-const regTab = document.querySelector('#reg-tab');
-const loginHeadBtn = document.querySelector('.login-tab');
-const regHeadBtn = document.querySelector('.reg-tab');
+const modal = $('.modal');
+// const userMethods = $('.user-methods');
+const loginBtn = $('#login-btn');
+const regBtn = $('#reg-btn');
+const modalClose = $('.modal-close');
+const userMethods = $('.user-methods');
+const loginTab = $('#login-tab');
+const regTab = $('#reg-tab');
+const loginHeadBtn = $('.login-tab');
+const regHeadBtn = $('.reg-tab');
 const activeTab = 'tab-active';
 
 function reset() {
-    modal.style.display = 'none';
-    regTab.style.display = 'none';
-    loginTab.style.display = 'none';
+    modal.css({
+        display: 'none',
+    });
+    regTab.css({
+        display: 'none',
+    });
+    loginTab.css({
+        display: 'none',
+    });
 }
-loginBtn.onclick = () => {
-    modal.style.display = 'block';
-    loginHeadBtn.classList.add(activeTab);
-    regHeadBtn.classList.remove(activeTab);
-    loginTab.style.display = 'block';
-    regTab.style.display = 'none';
-}
+loginBtn.click(() => {
+    modal.css({
+        display: 'block',
+    });
+    loginHeadBtn.addClass(activeTab);
+    regHeadBtn.removeClass(activeTab);
+    loginTab.css({
+        display: 'flex',
+    });
+    regTab.css({
+        display: 'none',
+    });
+});
 
-regBtn.onclick = () => {
-    modal.style.display = 'block';
-    regHeadBtn.classList.add(activeTab);
-    loginHeadBtn.classList.remove(activeTab);
-    regTab.style.display = 'block';
-    loginTab.style.display = 'none';
-}
+regBtn.click(() => {
+    modal.css({
+        display: 'block',
+    });
+    regHeadBtn.addClass(activeTab);
+    loginHeadBtn.removeClass(activeTab);
+    regTab.css({
+        display: 'flex',
+    });
+    loginTab.css({
+        display: 'none',
+    });
+});
 
-loginHeadBtn.onclick = () => {
-    loginHeadBtn.classList.add(activeTab);
-    regHeadBtn.classList.remove(activeTab);
-    loginTab.style.display = 'block';
-    regTab.style.display = 'none';
-}
-regHeadBtn.onclick = () => {
-    regHeadBtn.classList.add(activeTab);
-    loginHeadBtn.classList.remove(activeTab);
-    regTab.style.display = 'block';
-    loginTab.style.display = 'none';
-}
+loginHeadBtn.click(() => {
+    loginHeadBtn.addClass(activeTab);
+    regHeadBtn.removeClass(activeTab);
+    loginTab.css({
+        display: 'flex',
+    });
+    regTab.css({
+        display: 'none',
+    });
+});
+regHeadBtn.click(() => {
+    regHeadBtn.addClass(activeTab);
+    loginHeadBtn.removeClass(activeTab);
+    regTab.css({
+        display: 'flex',
+    });
+    loginTab.css({
+        display: 'none',
+    });
+});
 
-modalClose.onclick = () =>  {
+modalClose.click(() => {
     reset();
-}
-window.onclick = (event) => {
-    if(event.target == modal) {
-        reset();
-    }
-}
+});
+$('.modal-background').click(e => {
+    reset();
+});
 
 
-const cartItems = document.querySelectorAll('.cart-item');
-const cartDetails = document.querySelector('.cart-details');
-const productTitle = document.querySelector('#no-product');
-const cartTable = document.querySelector('#on-stock');
-function miniCartTab() {
-    if (cartItems.length > 3) {
-        for (let i = 3; i < cartItems.length; i++) {
-            cartItems[i].style.display = "none";
-        }
-        cartDetails.style.display = "block";
-    } else cartDetails.style.display = "none";
 
-}
-miniCartTab();
-function deleteItem(item) {
-    item.parentElement.remove();
-}
-function productShowTitle() {
-    if(cartItems.length !== 0) {
-        cartTable.style.display = "block";
-        productTitle.style.display = "none";
-    } else {
-        cartTable.style.display = "none";
-        productTitle.style.display = "block";
-    }
-}
-productShowTitle();
 var slideIndex = 0;
-const slides = document.querySelectorAll('.adver-img');
-console.log(slides);
-if(slides.length > 0)
+const slides = $('.adver-img');
+if (slides.length > 0)
     showSlides();
-
 function showSlides() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    $.each(slides, (index) => {
+        $(slides[index]).css({
+            display: 'none',
+        });
+    });
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    slides[slideIndex - 1].style.display = "inline";
+    $(slides[slideIndex - 1]).css({
+        display: 'inline',
+    });
     setTimeout(showSlides, 3000);
 }
 function pushSlides(n) {
@@ -106,13 +106,18 @@ function currentSlide(n) {
 }
 function showSlide(n) {
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "inline";
+    $.each(slides, index => {
+        $(slides[index]).css({
+            display: 'none',
+        });
+    });
+
+    $(slides[slideIndex - 1]).css({
+        display: 'inline',
+    });
 }
